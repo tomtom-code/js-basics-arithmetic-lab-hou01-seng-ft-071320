05261288bd41eef885aff91441446d9786b597f9
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-09-06 17:28:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here
+let var1 = 31;
+let var2 = 2 ;
+let multiply = var1*var2
+
+let random = Math.floor(Math.random()*10) + 1;
+
+let mod = 16 % 6;
+
+let max = Math.max(1,4,6,20);
